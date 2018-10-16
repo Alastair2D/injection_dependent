6cb9511ffafa03a48a1600c692bf5b691ba6e9a9
Harry <harryjp1995@gmail.com>
2018-10-16 11:24:51 +0100

    test: adds failing tests for app

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,11 +1,18 @@
-import {shallow} from 'enzyme';
-import React from 'react';
-import App from '../App';
-import { equal } from 'assert';
+import { shallow } from "enzyme";
+import React from "react";
+import App from "../App";
+import CurrentSite from "../src/CurrentSite";
+import PreviousSite from "../src/PreviousSite";
+import { equal } from "assert";
 
-describe('App', () => {
-  it('renders something', () => {
-    let wrapper = shallow(<App />);
-    expect(wrapper.length > 0).toBe(true);
+describe("App", () => {
+  it("renders a current site component", () => {
+    let wrapper = shallow(<CurrentSite />);
+    expect(wrapper.find("<CurrentSite />").length).toEqual(1);
+  });
+
+  it("renders a previous site compoent", () => {
+    let wrapper = shallow(<PreviousSite />);
+    expect(wrapper.find("<PreviousSite />").length).toEqual(1);
   });
 });
