f47970fe3138ed91ebe328952a3e193a539caf76
Harry <harryjp1995@gmail.com>
2018-10-16 15:19:40 +0100

    feat: adds the button to ConfirmButton; removes typo

--- src/ConfirmButton.js
+++ src/ConfirmButton.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Text, View, Button } from "react-native";
+import { Button } from "react-native";
 
-class ConfirmButton extends Comppnent {
+class ConfirmButton extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -11,6 +11,10 @@ class ConfirmButton extends Comppnent {
     event.preventDefault();
     const { onSubmit } = this.props;
     onSubmit();
+  }
+
+  render() {
+    return <Button onPress={this.handleSubmit} title="Confirm" />;
   }
 }
 
