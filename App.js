62c378802fe16859d7d5de783fad412d49618ad9
Harry <harryjp1995@gmail.com>
2018-10-16 11:34:49 +0100

    feat: tests now passing, componenets have been integrated with APP

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import CurrentSite from "./src/CurrentSite";
+import PreviousSite from "./src/PreviousSite";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to React Native!</Text>
+        <CurrentSite />;<PreviousSite />;
       </View>
     );
   }
--- __test__/app.test.js
+++ __test__/app.test.js
@@ -3,16 +3,15 @@ import React from "react";
 import App from "../App";
 import CurrentSite from "../src/CurrentSite";
 import PreviousSite from "../src/PreviousSite";
-import { equal } from "assert";
 
 describe("App", () => {
   it("renders a current site component", () => {
-    let wrapper = shallow(<CurrentSite />);
-    expect(wrapper.find("<CurrentSite />").length).toEqual(1);
+    let wrapper = shallow(<App />);
+    expect(wrapper.containsMatchingElement(<CurrentSite />)).toEqual(true);
   });
 
   it("renders a previous site compoent", () => {
-    let wrapper = shallow(<PreviousSite />);
-    expect(wrapper.find("<PreviousSite />").length).toEqual(1);
+    let wrapper = shallow(<App />);
+    expect(wrapper.containsMatchingElement(<PreviousSite />)).toEqual(true);
   });
 });
