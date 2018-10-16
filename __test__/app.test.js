e850250b71545193f867d13a923bbdba2b67db26
Andrew Wood <andrew.wood.dev@outlook.com>
2018-10-16 10:17:28 +0100

    Setup the file

---
+++ __test__/app.test.js
@@ -0,0 +1,13 @@
+import {mount} from 'enzyme';
+import React from 'react';
+import App from '../src/App';
+
+describe('App', () => {
+  describe('#Text', () => {
+    it('should render the the text Welcome to React Native! in our first text tag', () => {
+      const app = mount(<App/>);
+      const text = app.find('Text').at(0).text();
+      expect(text).toEqual('Welcome to React Native!');
+    });
+  });
+});
