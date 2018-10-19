60c625a60426f1b6d40d6e87a5f5653ddc7f750c
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-10-19 15:14:12 +0100

    feat: add checkboxes for legs and arms

--- __test__/SettingsView.test.js
+++ __test__/SettingsView.test.js
@@ -1,14 +1,31 @@
 import {shallow} from 'enzyme';
 import React from 'react';
 import SettingsView from '../components/SettingsView';
+import { CheckBox } from 'react-native-elements';
 
 describe('Sites', () => {
   it('renders the title of the list', () => {
     const app = shallow(<SettingsView />);
     const text = app
-    .find("#title")
+    .find("#sites")
     .dive()
     .text();
     expect(text).toEqual("Sites");
+  });
+
+  it('changes the state for legs checkbox', () => {
+    const app = shallow(<SettingsView />);
+    app.find("#legs").simulate('press');
+    const checkBox = app.find(CheckBox).at(0);
+    expect(checkBox.length).toEqual(1);
+    expect(checkBox.props().legsChecked).toEqual(true);
+  });
+
+  it('changes the state for arms checkbox', () => {
+    const app = shallow(<SettingsView />);
+    app.find("#arms").simulate('press');
+    const checkBox = app.find(CheckBox).at(1);
+    expect(checkBox.length).toEqual(1);
+    expect(checkBox.props().armsChecked).toEqual(true);
   });
 });
--- components/SettingsView.js
+++ components/SettingsView.js
@@ -3,10 +3,43 @@ import { StyleSheet, Text, View } from 'react-native';
 import { CheckBox } from 'react-native-elements';
 
 class SettingsView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      legsChecked: false,
+      armsChecked: false,
+      abdomenChecked: false,
+      buttocksChecked: false,
+      thighsChecked: false,
+    };
+
+    this.check = this.check.bind(this);
+  }
+
+  check() {
+    this.setState({legsChecked: !this.state.checked});
+    this.setState({armsChecked: !this.state.checked});
+  }
+
   render() {
     return (
       <View>
-      <Text id="title">Sites</Text>
+      <Text id="sites">Sites</Text>
+      <CheckBox
+        id="legs"
+        title="legs"
+        onPress={event => {
+          this.check()}}
+        legsChecked={this.state.legsChecked}
+        />
+
+      <CheckBox
+        id="arms"
+        title="arms"
+        onPress={event => {
+          this.check()}}
+        armsChecked={this.state.armsChecked}
+      />
       </View>
     )
   }
