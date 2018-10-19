fe4b07e1b7dc756c66802b22f31f446b94abebfa
Harry <harryjp1995@gmail.com>
2018-10-19 12:42:14 +0100

    chore: Adds react-native-table to package.json

--- __test__/HistoryScreen.test.js
+++ __test__/HistoryScreen.test.js
@@ -1,22 +1,18 @@
 import { shallow } from "enzyme";
+import { Text } from "react-native";
 import React from "react";
-import HistoryScreen from '../screens/HistoryScreen'
+import HistoryScreen from "../screens/HistoryScreen";
 
 import CurrentSite from "../components/CurrentSite";
 
 describe("HistoryScreen", () => {
   beforeEach(() => {
-    historyscreen = shallow(<HistoryScreen />)
-  })
-  
-  describe('Components', () => )
-    it("renders a current site component", () => {
-      expect(historyscreen.containsMatchingElement(<CurrentSite />)).toEqual(true);
-  })
+    historyscreen = shallow(<HistoryScreen />);
+  });
 
-  
-
-
-
-
-})
+  describe("Components", () => {
+    it("renders a text component", () => {
+      expect(historyscreen.containsMatchingElement(<Text />).toEqual(true));
+    });
+  });
+});
