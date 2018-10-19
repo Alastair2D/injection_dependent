7e5ea997db3c319ff386af0de8f5cd3e7657776c
Harry <harryjp1995@gmail.com>
2018-10-19 13:47:01 +0100

    feat: Adds HistoryTable component

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,29 +1,15 @@
 import React from "react";
 import { ScrollView, StyleSheet, Text } from "react-native";
-import CurrentSite from "../components/CurrentSite";
+import HistoryTable from "../components/HistoryTable";
 
 export default class HistoryScreen extends React.Component {
   static navigationOptions = {
     title: "History"
   };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      exampleData: ['{site: Right Arm, time: 9:47 }', '{site: Left Leg, time: 10:47 }' ],
-
-    };  
-  }
-
-
   render() {
     return (
       <ScrollView style={styles.container}>
-
-      <CurrentSite />
-        <Text> {this.state.exampleData} 
-        </Text>   
-      
+        <HistoryTable />
       </ScrollView>
     );
   }
