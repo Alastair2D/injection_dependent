39f1909893d002e520cd08c07a9c0fd48514e461
Harry <harryjp1995@gmail.com>
2018-10-20 23:27:52 +0100

    test: Unit test for comfirmation

--- __test__/HomeScreen.test.js
+++ __test__/HomeScreen.test.js
@@ -1,86 +1,101 @@
-import { shallow } from "enzyme";
-import React from "react";
-import { Button } from "react-native";
-import moment from "moment";
-import timekeeper from "timekeeper";
-import HomeScreen from "../screens/HomeScreen";
-import CurrentSite from "../components/CurrentSite";
-import PreviousSite from "../components/PreviousSite";
-import injectionsites from "../components/injectionsites";
-import BodyImages from "../components/BodyImages";
+import { shallow } from 'enzyme';
+import React from 'react';
+import { Button } from 'react-native';
+import moment from 'moment';
+import timekeeper from 'timekeeper';
+import HomeScreen from '../screens/HomeScreen';
+import CurrentSite from '../components/CurrentSite';
+import PreviousSite from '../components/PreviousSite';
+import injectionsites from '../components/injectionsites';
+import BodyImages from '../components/BodyImages';
 
-describe("Homescreen", () => {
+describe('Homescreen', () => {
   timekeeper.freeze(new Date(1539760000000));
   let hs;
   beforeEach(() => {
     hs = shallow(<HomeScreen />);
   });
-  it("renders a current site component", () => {
+  it('renders a current site component', () => {
     expect(hs.containsMatchingElement(<CurrentSite />)).toEqual(true);
   });
 
-  it("renders a previous site component", () => {
+  it('renders a previous site component', () => {
     expect(hs.containsMatchingElement(<PreviousSite />)).toEqual(true);
   });
 
-  describe("Recommended Site", () => {
-    it("should render the text of the suggested injection location", () => {
+  describe('Recommended Site', () => {
+    it('should render the text of the suggested injection location', () => {
       const currentSite = hs.find(CurrentSite);
       expect(currentSite.length).toEqual(1);
       expect(currentSite.props().site).toEqual(injectionsites[0]);
     });
   });
 
-  // describe("Confirm Button", () => {
-  //   beforeEach(() => {
-  //     window.alert = jest.fn();
-  //   });
-  //   it("should change the site after pressing Confirm", () => {
-  //     hs.find("#confirm").simulate("press");
-  //     const currentSite = hs.find(CurrentSite);
-  //     expect(currentSite.length).toEqual(1);
-  //     expect(currentSite.props().site).toEqual(injectionsites[1]);
-  //   });
-  //   it("shows the alert when confirmed", () => {
-  //     hs.find("#confirm").simulate("press");
-  //     expect(window.alert).toHaveBeenCalledWith("Confirmed");
-  //   });
-  //   it("should change the site again after pressing Confirm a second time", () => {
-  //     hs.find("#confirm").simulate("press");
-  //     hs.find("#confirm").simulate("press");
-  //     const currentSite = hs.find(CurrentSite);
-  //     expect(currentSite.length).toEqual(1);
-  //     expect(currentSite.props().site).toEqual(injectionsites[2]);
-  //   });
-  // });
+  describe('Confirmation', () => {
+    it('swipe right to confirm', () => {
+      hs.instance().onSwipeRight();
+      const currentSite = hs.find(CurrentSite);
+      expect(currentSite.length).toEqual(1);
+      expect(currentSite.props().site).toEqual(injectionsites[1]);
+    })
+    it('should change the site after confirmation', () => {
+      hs.instance().handleConfirmation();
+      const currentSite = hs.find(CurrentSite);
+      expect(currentSite.length).toEqual(1);
+      expect(currentSite.props().site).toEqual(injectionsites[1]);
+    });
+    it('should change the site again after pressing Confirm a second time', () => {
+      hs.instance().handleConfirmation();
+      hs.instance().handleConfirmation();
+      const currentSite = hs.find(CurrentSite);
+      expect(currentSite.length).toEqual(1);
+      expect(currentSite.props().site).toEqual(injectionsites[2]);
+    });
+  });
 
-  describe("History", () => {
-    it("should know the previous injection site once confirmed", () => {
+  xdescribe('Skipping', () => {
+    it('handleSkip', () => {
+      hs.instance().handleSkip();
+      const currentSite = hs.find(CurrentSite);
+      expect(currentSite.length).toEqual(1);
+      expect(currentSite.props().site).toEqual(injectionsites[1]);
+    });
+    it('doesn\'t add the suggested site to history', () => {
+      const pS = hs.find(PreviousSite).props().site.part;
+      hs.instance().handleSkip();
+      hs.instance().handleSkip();
+      hs.instance().handleSkip();
+      expect(hs.instance().state.history[0].site.part === pS).toEqual(true);
+    });
+  })
+
+  describe('History', () => {
+    it('should know the previous injection site once confirmed', () => {
       time = moment();
-      hs.find("#confirm").simulate("press");
+      hs.instance().handleConfirmation();
       const previousSite = hs.find(PreviousSite);
       expect(previousSite.length).toEqual(1);
       expect(previousSite.props().site).toEqual(injectionsites[0].part);
       expect(previousSite.props().time).toEqual(time);
     });
-    it("should update previous injection site once confirmed again", () => {
-      hs.find("#confirm").simulate("press");
+    it('should update previous injection site once confirmed again', () => {
+      hs.instance().handleConfirmation();
       time = moment();
-      hs.find("#confirm").simulate("press");
+      hs.instance().handleConfirmation();
       const previousSite = hs.find(PreviousSite);
       expect(previousSite.length).toEqual(1);
       expect(previousSite.props().site).toEqual(injectionsites[1].part);
       expect(previousSite.props().time).toEqual(time);
     });
-    // it("should start with a blank history", () => {
+    // it('should start with a blank history', () => {
     //   const hs = shallow(<hs />);
     //   const text = hs
-    //     .find("#previousSite")
+    //     .find('#previousSite')
     //     .dive()
     //     .find('#site')
     //     .dive()
     //     .text();
-    //   expect(text).toEqual("");
+    //   expect(text).toEqual('');
     // });
   });
 });
