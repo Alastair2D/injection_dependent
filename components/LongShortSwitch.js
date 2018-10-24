674c6c220ea3668dddecbe57d442cfe9765a03c7
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-10-24 15:38:22 +0100

    feat: toggle blackened and centred and default L/S changed

--- components/LongShortSwitch.js
+++ components/LongShortSwitch.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { ToggleSwitch } from 'toggle-switch-react-native';
+import { View, StyleSheet } from 'react-native';
+import ToggleSwitch from 'toggle-switch-react-native';
 
 class LongShortSwitch extends Component {
   constructor(props) {
     super(props);
     this.state = {
       medType: 'Short',
-      switchStatus: true
+      switchStatus: false
     };
   }
 
   flickSwitch = (status) => {
     if (status === true) {
       this.setState({
-        medType: 'Short',
+        medType: 'Long',
         switchStatus: true
       });
     } else {
       this.setState({
-        medType: 'Long',
+        medType: 'Short',
         switchStatus: false
       });
     }
@@ -28,19 +28,30 @@ class LongShortSwitch extends Component {
   render() {
     const tS = <ToggleSwitch
       isOn={this.state.switchStatus}
-      onColor='green'
-      offColor='red'
-      label={this.state.insulinType}
+      onColor='black'
+      offColor='black'
+      label={this.state.medType}
       labelStyle={{ color: 'black', fontWeight: '900' }}
       size='large'
       onToggle={() => this.flickSwitch(!this.state.switchStatus)}
     />
     return (
-      <View>
+      <View style={styles.container}> 
         {tS}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+
 
 export default LongShortSwitch;
