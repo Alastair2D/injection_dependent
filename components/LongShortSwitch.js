0be690cfcf0a2350f7c6d0bffbb53e5b941518cc
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-10-24 15:31:07 +0100

    chore: change insulinType to medType

--- components/LongShortSwitch.js
+++ components/LongShortSwitch.js
@@ -6,7 +6,7 @@ class LongShortSwitch extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      insulinType: 'Short',
+      medType: 'Short',
       switchStatus: true
     };
   }
