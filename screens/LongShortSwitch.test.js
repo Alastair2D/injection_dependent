9ebffce079b6f3ad8481aac9f8c593f19ab53444
Harry <harryjp1995@gmail.com>
2018-10-24 09:19:09 +0100

    test: Adds failing tests for new switch component

--- screens/LongShortSwitch.test.js
+++ screens/LongShortSwitch.test.js
@@ -2,12 +2,26 @@ import { shallow } from 'enzyme';
 import React from 'react';
 
 import LongShortSwitch from '../components/LongShortSwitch';
+import ToggleSwitch from 'toggle-switch-react-native';
 
 describe('LongShortSwitch', () => {
+  let lSS;
+  beforeEach(() => {
+    lSS = <LongShortSwitch />;
+  });
+  it('is on by default', () => {
+    const toggleSwitch = lSS.find(ToggleSwitch);
+    expect(toggleSwitch.state.isOn).toEqual('On');
+  })
   it('sets state to short-acting when on', () => {
-    
+    const toggleSwitch = lSS.find(ToggleSwitch);
+    toggleSwitch.simulate('click');
+    toggleSwitch.simulate('click');
+    expect(toggleSwitch.state.isOn == 'On').toEqual(true);
   });
   it('sets state to long-acting when off', () => {
-
+    const toggleSwitch = lSS.find(ToggleSwitch);
+    toggleSwitch.simulate('click');
+    expect(toggleSwitch.state.isOn == 'Off').toEqual(true);
   });
 });
