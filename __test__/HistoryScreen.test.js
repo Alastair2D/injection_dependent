871c89c4563f0bab93f9c7fce71f7b4873a606d9
Andrew Wood <andrew.wood.dev@outlook.com>
2018-10-25 13:37:10 +0100

    Test: adds history screen tests for user name entry

--- __test__/HistoryScreen.test.js
+++ __test__/HistoryScreen.test.js
@@ -1,5 +1,5 @@
 import { shallow } from "enzyme";
-import { Text, Button } from "react-native";
+import { Text, Button, TextInput } from "react-native";
 import React from "react";
 import { HistoryScreen } from "../screens/HistoryScreen";
 import HistoryTable from "../components/HistoryTable";
@@ -11,7 +11,7 @@ describe("HistoryScreen", () => {
     historyScreen = shallow(<HistoryScreen history={'SomeMockHistory'} />);
   });
 
-  describe('Components', () => {
+  describe('HistoryTable', () => {
     it('renders a table component', () => {
       expect(historyScreen.containsMatchingElement(<HistoryTable />)).toEqual(true);
     });
@@ -20,6 +20,18 @@ describe("HistoryScreen", () => {
       expect(table.props().history).toEqual('SomeMockHistory')
     })
   });
+
+  describe('Username', () => {
+    it('should have a text label', () => {
+      expect(historyScreen.find(Text).dive().text()).toEqual('Username:')
+    })
+
+    it('should be able to enter user name in a text box', () => {
+      userInput = historyScreen.find(TextInput)
+      userInput.simulate('changeText', 'Bob')
+      expect(historyScreen.state().user_id).toEqual('Bob')
+    })
+  })
 
   describe('DB save and load', () => {
     beforeEach(() => {
