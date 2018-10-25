a36d92c096ae590026a59aabc1281750484dff92
Andrew Wood <andrew.wood.dev@outlook.com>
2018-10-25 11:32:26 +0100

    Test: add test for attaching actions to history screen props

--- __test__/ConnectedHistoryScreen.test.js
+++ __test__/ConnectedHistoryScreen.test.js
@@ -9,12 +9,12 @@ import { resetHistory } from '../redux/actions/history';
 
 describe("ConnectedHistoryScreen", () => {
   let historyscreen;
-  let store 
+  let store
   beforeEach(() => {
     store = createMockStore({
       history: [{
-        time: moment(), 
-        site: {part: "left buttock"} 
+        time: moment(),
+        site: {part: "left buttock"}
       }]
     });
     historyscreen = shallow(<HistoryScreen store={store} />);
@@ -24,8 +24,18 @@ describe("ConnectedHistoryScreen", () => {
     expect(historyscreen.props().history[0].site.part).toEqual("left buttock")
   });
 
-  // it("adds history reset action to props", () => {
-  //   historyscreen.props().resetHistory()
-  //   expect(store.isActionDispatched(resetHistory)).toBe(true);
-  // })
+  it("adds saveInj action to props", () => {
+    historyscreen.props().saveInj('test')
+    expect(store.getActions()).toEqual([{"item": "test", "type": "history-save-injection"}]);
+  })
+
+  it("adds updateSyncStatus action to props", () => {
+    historyscreen.props().updateSyncStatus()
+    expect(store.getActions()).toEqual([{"type": "history-sync-status"}]);
+  })
+
+  it("adds resetHistory action to props", () => {
+    homescreen.props().resetHistory()
+    expect(store.getActions()).toEqual([{"type": "history-reset-defaults"}]);
+  })
 });
