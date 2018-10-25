319f288d7932cded336cb139b400ba8ed53ccaf1
Andrew Wood <andrew.wood.dev@outlook.com>
2018-10-25 11:53:09 +0100

    Tests: some first history screen tests

--- __test__/HistoryScreen.test.js
+++ __test__/HistoryScreen.test.js
@@ -8,13 +8,17 @@ import mockAxios from "../__mocks__/axios"
 describe("HistoryScreen", () => {
   let historyScreen;
   beforeEach(() => {
-    historyScreen = shallow(<HistoryScreen />);
+    historyScreen = shallow(<HistoryScreen history={'SomeMockHistory'} />);
   });
 
   describe('Components', () => {
     it('renders a table component', () => {
       expect(historyScreen.containsMatchingElement(<HistoryTable />)).toEqual(true);
     });
+    it('passes the Table the history data', () => {
+      let table = historyScreen.find(HistoryTable)
+      expect(table.props().history).toEqual('SomeMockHistory')
+    })
   });
 
   describe('DB save and load', () => {
@@ -24,11 +28,15 @@ describe("HistoryScreen", () => {
     })
     it('saveData calls axios post', () => {
       historyScreen.find('#save').simulate('press')
-      expect(mockAxios.post).toHaveBeenCalledWith('http://localhost:9292/injections')
+      expect(mockAxios.post).toHaveBeenCalledWith(
+        'https://guarded-caverns-16437.herokuapp.com/injections'
+      )
     })
     it('loadData calls axios get', () => {
       historyScreen.find('#load').simulate('press')
-      expect(mockAxios.get).toHaveBeenCalledWith('http://localhost:9292/injections?user_id=1')
+      expect(mockAxios.get).toHaveBeenCalledWith(
+        'https://guarded-caverns-16437.herokuapp.com/injections?user_id=1'
+      )
     })
   });
 });
