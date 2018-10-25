30e6eda7f212ebc905237e3c9fc4e8641efad998
Harry <harryjp1995@gmail.com>
2018-10-25 09:59:08 +0100

    refactor: actions.test liniting issues

--- __test__/actions.test.js
+++ __test__/actions.test.js
@@ -6,56 +6,56 @@ describe('actions', () => {
     const injection = 'Dummy injection'
     const expectedAction = {
       type: 'history-save-injection',
-      item: injection
-    }
+      item: injection,
+    };
     expect(saveInj(injection)).toEqual(expectedAction)
-  })
+  });
 
   it('should create an action to reset history', () => {
     const expectedAction = {
-      type: 'history-reset-defaults'
-    }
-    expect(resetHistory()).toEqual(expectedAction)
-  })
+      type: 'history-reset-defaults',
+    };
+    expect(resetHistory()).toEqual(expectedAction);
+  });
 
   it('should create an action to change dbsync status to true', () => {
     const expectedAction = {
-      type: 'history-sync-status'
+      type: 'history-sync-status',
     }
-    expect(updateSyncStatus()).toEqual(expectedAction)
+    expect(updateSyncStatus()).toEqual(expectedAction);
   });
 
   it('should create an action to move to next inj site', () => {
     const expectedAction = {
-      type: 'sites-next-injection-site'
-    }
-    expect(nextInjSite()).toEqual(expectedAction)
-  })
+      type: 'sites-next-injection-site',
+    };
+    expect(nextInjSite()).toEqual(expectedAction);
+  });
 
   it('should create an action to rotate injection sites by n', () => {
-    const n = 5
+    const n = 5;
     const expectedAction = {
       type: 'sites-rotate-n-sites',
-      number: n
-    }
-    expect(rotateNSites(n)).toEqual(expectedAction)
-  })
+      number: n,
+    };
+    expect(rotateNSites(n)).toEqual(expectedAction);
+  });
 
   it('should create an action to reset injection sites', () => {
     const expectedAction = {
-      type: 'sites-reset-defaults'
-    }
+      type: 'sites-reset-defaults',
+    };
     expect(resetSites()).toEqual(expectedAction)
-  })
+  });
 
   it('should create an action to change the state of the checkbox', () => {
-    const part = 'Thigh'
-    const previousCheckedStatus = true
+    const part = 'Thigh';
+    const previousCheckedStatus = true;
     const expectedAction = {
       type: 'sites-checked',
       part: part,
       previousCheckedStatus: previousCheckedStatus
     }
     expect(checkSites(part, previousCheckedStatus)).toEqual(expectedAction)
-  })
-})
+  });
+});
