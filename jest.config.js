f77a262651b9a1d73189020d3fce9dc00981de4b
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-10-29 13:26:57 +0000

    add images

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testMatch: [
+    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
+  ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/']
+};
